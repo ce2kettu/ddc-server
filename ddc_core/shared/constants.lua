81118d0c9e4957f3ceefcae5fddcd6df2e75c7df
ce2kettu <teemu.ket@gmail.com>
2020-03-28 01:37:02 +0200

    Add core constants

---
+++ ddc_core/shared/constants.lua
@@ -0,0 +1,10 @@
+g_isServer = (triggerClientEvent ~= nil)
+g_isClient = (not g_isServer)
+
+g_serverName = "mgm"
+g_serverFPSLimit = 51
+g_serverVersion = "1.0.0 dev"
+g_serverColor = {
+	hex	= "#ff7ba1",
+	rgb	= {255, 123, 161}
+}
