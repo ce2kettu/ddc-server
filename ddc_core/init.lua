609242aaf594014b515b2cc3fc8a682161a936d1
ce2kettu <teemu.ket@gmail.com>
2020-03-28 01:38:58 +0200

    Add core init and meta

--- ddc_core/init.lua
+++ ddc_core/init.lua
@@ -1,9 +1,8 @@
-local function onResourceStart(uResource)
-	if(Core) then
+local function onResourceStart()
+	if (Core) then
 		Core:new()
 	end
 end
-
 
 local function onResourceStop()
 	if (Core) then
@@ -11,5 +10,5 @@ local function onResourceStop()
 	end
 end
 
-addEventHandler((g_bServer and "onResourceStart" or "onClientResourceStart"), resourceRoot, onResourceStart)
-addEventHandler((g_bServer and "onResourceStop" or "onClientResourceStop"), resourceRoot, onResourceStop)
+addEventHandler((g_isServer and "onResourceStart" or "onClientResourceStart"), resourceRoot, onResourceStart)
+addEventHandler((g_isServer and "onResourceStop" or "onClientResourceStop"), resourceRoot, onResourceStop)
