9a310c2f6440c962b14cd657cedb6a25c8253657
ce2kettu <teemu.ket@gmail.com>
2020-03-31 00:31:06 +0300

    Convert ui

--- ddc_raceui/countdown.lua
+++ ddc_raceui/countdown.lua
@@ -3,10 +3,6 @@ local COUNTDOWN_WIDTH, COUNTDOWN_HEIGHT = 250, 250
 local START_X = (SCREEN_WIDTH / 2) - (COUNTDOWN_WIDTH / 2)
 local START_Y = (SCREEN_HEIGHT / 3) - (COUNTDOWN_HEIGHT / 2)
 local FADE_SPEED = 0.2
-local tocolor_ = tocolor
-local math_max = math.max
-local math_min = math.min
-local getTickCount_ = getTickCount
 
 Countdown = {}
 
@@ -32,26 +28,26 @@ function Countdown:render()
     if (self.isFinished) then return end
 
     if (self.lastTick) then
-        self.animationDelta = getTickCount_() - self.lastTick
-        self.animationDelta = math_min(self.animationDelta, (1 / 60) * 1000)
+        self.animationDelta = getTickCount() - self.lastTick
+        self.animationDelta = math.min(self.animationDelta, (1 / 60) * 1000)
     end
-    
-    self.lastTick = getTickCount_()
 
-    if (getTickCount_() - self.currentTick > 1000) then
-        
-        self.currentTick = getTickCount_()
+    self.lastTick = getTickCount()
+
+    if (getTickCount() - self.currentTick > 1000) then
+
+        self.currentTick = getTickCount()
         self.currentCount = self.currentCount - 1
-        
+
         if (self.currentCount > self.minCount) then
             self.alpha = 255
             playSound("assets/sounds/cd.wav")
         elseif (self.currentCount == self.minCount) then
             self.alpha = 255
             playSound("assets/sounds/cd0.wav")
-        else 
+        else
             self.currentCount = self.minCount
-        
+
             if (self.alpha <= 0) then
                 self.isFinished = true
                 removeEventHandler("onClientRender", root, self._onClientRender)
@@ -59,7 +55,7 @@ function Countdown:render()
             end
         end
     end
-    
+
     self.alpha = self.alpha - FADE_SPEED * self.animationDelta
-    dxDrawImage(START_X, START_Y, COUNTDOWN_WIDTH, COUNTDOWN_HEIGHT, "assets/images/countdown/cd"..self.currentCount..".png", 0, 0, 0, tocolor_(255, 255, 255, math_max(0, self.alpha)), false)
+    dxDrawImage(START_X, START_Y, COUNTDOWN_WIDTH, COUNTDOWN_HEIGHT, "assets/images/countdown/cd"..self.currentCount..".png", 0, 0, 0, tocolor(255, 255, 255, math.max(0, self.alpha)), false)
 end
