0458758bf91d888a4277fa55be89704b8a16b30f
ce2kettu <teemu.ket@gmail.com>
2020-04-04 21:22:41 +0300

    Remove useless stuff from notifications

--- ddc_notifications/client.lua
+++ ddc_notifications/client.lua
@@ -3,11 +3,3 @@ local RESOURCE_NAME = getResourceName(getThisResource())
 
 uiImportScript(RESOURCE_NAME.."/NotificationProvider.lua")
 uiRegisterComponent("DxNotification", RESOURCE_NAME.."/Notification.lua")
-
-local t = DxRoundedRectDetail:new(200, 200, 300, 30, 15, tocolor(0, 0, 0, 150), "[DM]Vortex_-Vol11-Time_Vortex's",
-    "files/images/current_map.png", 18, 18, tocolor(74, 198, 240, 255))
-
-addCommandHandler("test2", function(c, t)
-    local notif = uiCreateElement("DxNotification", "success", "Success", 
-    "This is a success alert — check it out! What the fuck is wrong with me? I don't know. But today is a good day!", 5000)
-end)
